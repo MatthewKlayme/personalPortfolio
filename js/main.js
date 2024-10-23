f3f7463456163d00cb08b2d178f731c2bf7fd699
Rishabh Kumar <68524799+SoloBoyy@users.noreply.github.com>
2024-10-23 11:55:57 -0500

    revert

--- js/main.js
+++ js/main.js
@@ -17,22 +17,15 @@
   });
 
 
-  var fullHeight = function() {
-    if ($(window).width() > 767) {  // Only apply for larger screens
-        $('.js-fullheight').css('height', $(window).height());
-        $(window).resize(function() {
-            if ($(window).width() > 767) {
-                $('.js-fullheight').css('height', $(window).height());
-            } else {
-                $('.js-fullheight').css('height', 'auto');  // Allow auto height on mobile
-            }
-        });
-    } else {
-        $('.js-fullheight').css('height', 'auto');  // Set height to auto on mobile initially
-    }
-};
-fullHeight();
+	var fullHeight = function() {
 
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
 
 	// loader
 	var loader = function() {
